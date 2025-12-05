bfdead020a646fbfd4ea50894cbeb22841e26bab
Pietro-Calvi <pietro.calvi@studenti.unitn.it>
2025-12-05 15:24:28 +0100

    First overview

---
+++ src/ciuc_planet.rs
@@ -0,0 +1,136 @@
+use std::marker::PhantomData;
+use std::sync::mpsc;
+use common_game::components::planet::{Planet, PlanetAI, PlanetState, PlanetType};
+use common_game::components::resource::{Combinator, Generator, BasicResorceType};
+use common_game::components::rocket::Rocket;
+use common_game::protocols::messages;
+
+struct SafeState;
+struct StatisticState;
+
+// Group-defined AI struct
+pub struct AI<T> { /* your AI state here */
+    state: PhantomData<T>,
+    number_explorers: usize
+}
+
+impl AI<SafeState> {
+    fn generate_carbon(){
+        println!("Generating carbon safe");
+    }
+}
+
+impl AI<StatisticState> {
+    fn generate_carbon(){
+        println!("Generating carbon statistically");
+    }
+}
+
+
+impl PlanetAI for AI {
+    fn handle_orchestrator_msg(
+        &mut self,
+        state: &mut PlanetState,
+        generator: &Generator,
+        combinator: &Combinator,
+        msg: messages::OrchestratorToPlanet
+    ) -> Option<messages::PlanetToOrchestrator> {
+
+        match msg {
+            messages::OrchestratorToPlanet::Sunray() => {
+                //Se ho una cella scarica la carico
+
+                // aggiorno il numero di sunray
+
+                //se sono in stato safe e ho abbastanza dati entro in stato statistico
+            }
+            messages::OrchestratorToPlanet::InternalStateRequest() => {
+                //Restituisco state
+            }
+            messages::OrchestratorToPlanet::IncomingExplorerRequest(_) => {
+                //Sostituisco il nuovo sender e gli restituisco il mio
+            }
+            messages::OrchestratorToPlanet::OutgoingExplorerRequest(_) => {
+                //Elimino il mio sender per explorer
+            }
+
+        }
+
+        None
+    }
+
+    fn handle_explorer_msg(
+        &mut self,
+        state: &mut PlanetState,
+        generator: &Generator,
+        combinator: &Combinator,
+        msg: messages::ExplorerToPlanet
+    ) -> Option<messages::PlanetToExplorer> {
+
+        match msg {
+            messages::ExplorerToPlanet::SupportedResourceRequest (msg) => {
+                //Restituire Carbonio
+            }
+            messages::ExplorerToPlanet::SupportedCombinationRequest(msg) => {
+                //Restituire nessuna combination rule
+            }
+            messages::ExplorerToPlanet::GenerateResourceRequest(msg) => {
+                //Controllare che la risorsa sia corretta
+                self.generate_carbon();
+            }
+            messages::ExplorerToPlanet::CombineResourceRequest(msg) => {
+                //Restituire il nulla
+            }
+            messages::ExplorerToPlanet::AvailableEnergyCellRequest(msg) => {
+                //Restituire numero di energy cell available
+            }
+        }
+
+        None
+    }
+
+    fn handle_asteroid(
+        &mut self,
+        state: &mut PlanetState,
+        generator: &Generator,
+        combinator: &Combinator,
+    ) -> Option<Rocket> {
+
+        //Se non ho un rocket muoio
+
+        //se ho il rocket mi difendo e modifico il conteggio di aseteroidi
+
+        //se sono in stato safe e ho abbastanza dati entro in stato statistico
+
+        None
+    }
+
+    fn start(&mut self, state: &PlanetState) { /* startup code */ }
+    fn stop(&mut self, state: &PlanetState) { /* stop code */ }
+}
+
+// This is the group's "export" function. It will be called by
+// the orchestrator to spawn your planet.
+pub fn create_planet(
+    rx_orchestrator: mpsc::Receiver<messages::OrchestratorToPlanet>,
+    tx_orchestrator: mpsc::Sender<messages::PlanetToOrchestrator>,
+    rx_explorer: mpsc::Receiver<messages::ExplorerToPlanet>,
+    tx_explorer: mpsc::Sender<messages::PlanetToExplorer>,
+    id:u32
+) -> Planet<AI> {
+    let ai = AI {};
+
+    let gen_rules = vec![BasicResourceType::Carbon];
+    let comb_rules = vec![];
+
+    // Construct the planet and return yourit
+    Planet::new(
+        id,
+        PlanetType::A,
+        ai,
+        gen_rules,
+        comb_rules,
+        (rx_orchestrator, tx_orchestrator),
+        (rx_explorer, tx_explorer)
+    ).unwrap() // Don't call .unwrap()! You should do error checking instead.
+}
